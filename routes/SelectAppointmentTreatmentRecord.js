d7b65e5978b522e0b5585d0a6df76b82a258c9fe
Orion Koepke <orion.koepke@ttu.edu>
2017-04-12 14:36:34 -0500

    Updated ViewAppointmentTreatmentRecord.js
    I added functionality to this.

--- routes/SelectAppointmentTreatmentRecord.js
+++ routes/SelectAppointmentTreatmentRecord.js
@@ -9,15 +9,13 @@ router.get('/',function(req,res){
 
 router.post('/', function(req, res){
   Records.find({PatientSSN: req.body.patients}).then(function(ans){
-    console.log(req.body.patients);
     var records = [];
     for(var i = 0; i < ans.length; i++){
-      var record = {date: "", doctor: ""};
-      record.date = "DATE: " + ans[i].date;
-      record.ssn = ans[i].PatientSSN;
+      var record = {date: "", SSN: ""};
+      record.date = ans[i].date;
+      record.SSN = ans[i].PatientSSN;
       records[i] = record;
     }
-    console.log(records);
     return res.render('SelectAppointmentTreatmentRecord', { records: records });
   });
 });
