b9d50a3bc8283be91cfb2b1a36a7fbf628414439
Orion Koepke <orion.koepke@ttu.edu>
2017-04-23 05:51:49 -0500

    CreateNewPatient.ejs

--- routes/CreateNewPatient.js
+++ routes/CreateNewPatient.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var Patient = require('../models/Patients.js');
 var User = require('../models/Users.js');
 
-router.get('/',function(req,res){
+router.get('/', function(req, res){
   User.find({userType: "doctor"}).then(function(ans){
     var doctors = [];
     for(var i = 0; i < ans.length; i++)
@@ -15,6 +15,22 @@ router.get('/',function(req,res){
     }
     return res.render('CreateNewPatient', {doctors: doctors});
   });
+});
+
+router.post('/', function(req, res){
+  newPatient = new Patient();
+  newPatient.firstname = req.body.firstname;
+  newPatient.lastname = req.body.lastname;
+  newPatient.address = req.body.address;
+  newPatient.phone = req.body.phone;
+  newPatient.email = req.body.email;
+  newPatient.SSN = req.body.SSN;
+  newPatient.insurance = req.body.insurance;
+  newPatient.onFilePaymentInfo = req.body.onFilePaymentInfo;
+  newPatient.doctor = req.body.doctor;
+  newPatient.medicalNotes = req.body.medicalNotes;
+  newPatient.save();
+  return res.render('MainPage');
 });
 
 module.exports = router;
