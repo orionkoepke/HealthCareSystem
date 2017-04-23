5c4ef1a85ec0626f39a7375f134129d2dad669cc
Orion Koepke <orion.koepke@ttu.edu>
2017-04-23 05:28:19 -0500

    CreateNewPatient.js
    Added code to the get method

--- routes/CreateNewPatient.js
+++ routes/CreateNewPatient.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
 var Patient = require('../models/Patients.js');
+var User = require('../models/Users.js');
 
 router.get('/',function(req,res){
-  return res.render('CreateNewPatient');
+  User.find({userType: "doctor"}).then(function(ans){
+    return res.render('CreateNewPatient', {doctors: ans});
+  });
 });
 
 module.exports = router;
