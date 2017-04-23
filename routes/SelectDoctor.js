0fca4fa4307ca5bda46776728452a00ba50df8eb
Orion Koepke <orion.koepke@ttu.edu>
2017-04-23 09:02:03 -0500

    ViewSchedule.js

--- routes/SelectDoctor.js
+++ routes/SelectDoctor.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var User = require('../models/Users.js');
 
 router.get('/',function(req,res){
-  console.log("SelectDoctor.js get /");
     User.find({userType: "doctor"}).then(function(ans){
       var doctors = [];
       for(var i = 0; i < ans.length; i++){
@@ -13,7 +12,6 @@ router.get('/',function(req,res){
         doctor.doctorId = ans[i].doctor;
         doctors[i] = doctor;
       }
-      console.log("\tdoctors :: " + doctors);
       return res.render('SelectDoctor', { doctors: doctors });
     });
 });
--- routes/ViewSchedule.js
+++ routes/ViewSchedule.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 var router = express.Router();
-var User = require('../models/Users.js');
+var Record = require('../models/Records.js');
+var Patient = require('../models/Patients.js');
 
 router.get('/',function(req,res){
   return res.render('ViewSchedule');
+});
+
+router.post('/', function(req, res){
+  Patient.find({SSN: req.body.patients}).then(function(ans){
+    // This gets the previous day.
+    var prevDay = new Date((new Date).valueOf() - 86350989);
+
+      Record.find({doctor: ans[0].doctor, date: {$gte: prevDay}}).then(function(ans){
+        return res.render('ViewSchedule', {appointments: ans});
+      });
+  });
+
+  router.post('/add_appointment', function(req, res){
+    return res.render('ViewSchedule', {appointments: []]});
+  });
+
 });
 
 module.exports = router;
