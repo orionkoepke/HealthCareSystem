c81d033ba0897fcac3d3c2044855c0d534d679aa
orionkoepke <orion.koepke@ttu.edu>
2017-04-26 16:27:05 -0500

    Made ViewPatientRecord.js and ViewAppointmentTreatmentRecord.js Editable

--- routes/CreateNewPatient.js
+++ routes/CreateNewPatient.js
@@ -3,18 +3,27 @@ const mongoose = require('mongoose');
 var router = express.Router();
 var Patient = require('../models/Patients.js');
 var User = require('../models/Users.js');
+const CheckUserAuthorization = require('../modules/CheckUserAuthorization');
 
 router.get('/', function(req, res){
-  User.find({userType: "doctor"}).then(function(ans){
-    var doctors = [];
-    for(var i = 0; i < ans.length; i++)
-    {
-      var name = "Dr. " + ans[i].lastname;
-      var doctorID = ans[i].doctor;
-      doctors[i] = {name: name, doctorID: doctorID};
-    }
-    return res.render('CreateNewPatient', {doctors: doctors});
-  });
+  if(!req.session.user){
+    return res.render('LoginPage');
+  }
+  else if(!CheckUserAuthorization(req.session.user.userType,"CNPR")){
+    return res.render('MainPage',{ permissionError: "You do not have permission to do this."});
+  }
+  else{
+    User.find({userType: "doctor"}).then(function(ans){
+      var doctors = [];
+      for(var i = 0; i < ans.length; i++)
+      {
+        var name = "Dr. " + ans[i].lastname;
+        var doctorID = ans[i].doctor;
+        doctors[i] = {name: name, doctorID: doctorID};
+      }
+      return res.render('CreateNewPatient', {doctors: doctors});
+    });
+  }
 });
 
 router.post('/', function(req, res){
--- routes/ViewPatientRecord.js
+++ routes/ViewPatientRecord.js
@@ -7,11 +7,32 @@ router.get('/',function(req,res){
   return res.render('ViewPatientRecord');
 });
 
+var patientRecord;
+
 router.post('/', function(req, res){
   Patient.find({SSN: req.body.patients}).then(function(ans){
-    console.log(ans)
+    patientRecord = ans[0];
     return res.render('ViewPatientRecord', { patient: ans[0] });
   });
+});
+
+router.post('/update', function(req, res){
+
+  patientRecord.firstname = req.body.firstname;
+  patientRecord.lastname = req.body.lastname;
+  patientRecord.address = req.body.address;
+  patientRecord.phone = req.body.phone;
+  patientRecord.email = req.body.email;
+  patientRecord.SSN = req.body.SSN;
+  patientRecord.insurance = req.body.insurance;
+  patientRecord.onFilePaymentInfo = req.body.onFilePaymentInfo;
+  patientRecord.doctor = req.body.doctor;
+  patientRecord.medicalNotes = req.body.medicalNotes;
+
+  Patient.findByIdAndUpdate(patientRecord._id, { $set: patientRecord}, function(err, numAffected){});
+
+  patientRecord = null;
+  return res.redirect('/users');
 });
 
 module.exports = router;
